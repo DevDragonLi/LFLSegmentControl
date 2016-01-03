8de7e50e45369b1556f65e8bb78a8bbf68437073
LFL <dragonli_52171@163.com>
2016-01-03 14:40:13 +0800

    Initial Commit

---
+++ LFLSegmentControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LFLSegmentControl
+//
+//  Created by vintop_xiaowei on 16/1/3.
+//  Copyright © 2016年 vintop_DragonLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LFLSegmentControl/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LFLSegmentControl
+//
+//  Created by vintop_xiaowei on 16/1/3.
+//  Copyright © 2016年 vintop_DragonLi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
